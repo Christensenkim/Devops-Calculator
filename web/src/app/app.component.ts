d471c07c2da0634ed4906e5ab21cde00a1dc9e74
Christensenkim <55380386+Christensenkim@users.noreply.github.com>
2021-05-26 16:25:07 +0200

    test

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {CalculatorService} from './Shared/calculator.service';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,6 +10,13 @@ import {CalculatorService} from './Shared/calculator.service';
 export class AppComponent {
   title = 'calculator-web';
   screen = '';
+
+  constructor(private service: CalculatorService) {
+  }
+
+  test(): void {
+    console.log(this.service.test());
+  }
 
   entervalue(s: string): void {
   }
