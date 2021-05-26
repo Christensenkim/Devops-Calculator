a8a5a988f6bdf2f676d9033e34d952055e35c3ff
René Jørgensen <rene686f@easv365.dk>
2021-05-26 17:37:07 +0200

    Startup

--- web/src/app/calculator/calculator.component.ts
+++ web/src/app/calculator/calculator.component.ts
@@ -6,20 +6,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./calculator.component.scss']
 })
 export class CalculatorComponent implements OnInit {
+  constructor() { }
   screen: string | undefined;
+  number1: string | undefined;
+  number2: string | undefined;
+  calcCondition: string | undefined;
+  calcResult: number | undefined;
   private value: any;
 
-  constructor() { }
-
   ngOnInit(): void {
+    this.screen = '';
   }
 
   entervalue(value: any): void {
-    this.value = value;
-
+    if ((this.calcCondition === '+') || (this.calcCondition === '-') || (this.calcCondition === '*') || (this.calcCondition === '/')) {
+      this.calcResult = this.calcResult + value;
+      this.screen = this.screen + value;
+      this.number1 = this.screen;
+    }
+    else{
+      this.screen = this.screen + value;
+      this.number2 = this.screen;
+    }
   }
 
-  condition(s: string): void {
+  condition(value: any): void {
+    this.screen = this.screen + value;
+    this.calcCondition = value;
   }
 
   result(): void {
@@ -27,5 +40,9 @@ export class CalculatorComponent implements OnInit {
 
   clear(): void {
     this.screen = '';
+    this.calcCondition = '';
+    this.operand1 = '';
+    this.operand2 = '';
+    this.value = '';
   }
 }
