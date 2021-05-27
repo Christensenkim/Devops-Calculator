4537501d2bd52a420d2edc3d161b10d8d0fabcd2
René Jørgensen <rene686f@easv365.dk>
2021-05-27 16:23:02 +0200

    Update environment.ts

--- web/src/environments/environment.ts
+++ web/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  webAPIURL: 'https://localhost:44314/Calculator'
-  //webAPIURL: 'http://185.51.76.19:25001/calculator'
+  //webAPIURL: 'https://localhost:44314/Calculator'
+  webAPIURL: 'http://185.51.76.19:25001/calculator'
 };
